63aafa4050fe497f63f43de1ef19baa9c95d5303
PRASADCH257 <chandralasdvprasad@gmail.com>
2025-06-17 15:20:21 +0530

    Add files via upload

---
+++ OPerator.java
@@ -0,0 +1,14 @@
+class OPerator
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=10;
+
+System.out.println ("addition"+(a+b));
+System.out.println ("subtraction"+(a-b));
+System.out.println ("multiplication"+(a*b));
+System.out.println ("divide"+(a/b));
+System.out.println ("remainder"+(a%b));
+}
+}
---
+++ Unary.java
@@ -0,0 +1,20 @@
+class Unary
+{
+public static void main(String args[])
+{
+int a=5;
+int b=0;
+int x=++a;
+int y=++b;
+System.out.println("inctement"+x);
+System.out.println("inctement"+y);
+int c=0;
+int d=3;
+
+int v=--c;
+int z=--d;
+System.out.println("inctement"+v);
+System.out.println("inctement"+z);
+
+}
+}
