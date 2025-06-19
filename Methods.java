e63abec1311a49ace5354c9e6a4048c5d1c97bb8
PRASADCH257 <chandralasdvprasad@gmail.com>
2025-06-19 12:04:19 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,63 @@
+//without parameters without return value
+/* class Methods{
+public void Add()
+{
+System.out.println(10+3);
+}
+static public void main(String[] add)
+{
+Methods m=new Methods();
+m.Add();
+}
+}*/
+
+
+//without parameters with return value
+
+/*
+class Methods{
+public int Sub()
+{
+return(10-3);
+}
+static public void main(String[] sub)
+{
+Methods m=new Methods();
+int b=m.Sub();
+System.out.println(b);
+}
+}*/
+
+
+//with parameters with return value
+
+/*class Methods{
+public int Div(int a,int b)
+{
+return(a/b);
+}
+static public void main(String[] div)
+{
+Methods m=new Methods();
+int b=m.Div(3,3);
+System.out.println(b);
+}
+}*/
+
+//without parameters with return value
+
+/*
+class Methods
+{
+public void Mul(int a,int b)
+{
+System.out.println(a*b);
+}
+public static void main(String[] mul)
+{
+Methods m=new Methods();
+m.Mul(3,3);
+}
+}
+
+*?
